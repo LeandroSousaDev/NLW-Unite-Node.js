0809e8c58294676f88b2cae98d08ad1bdc894c8c
LeandroSousaDev <leandro_s.sousa@hotmail.com>
2024-04-14 00:19:07 -0300

    Aula 2 parcial -2

---
+++ src/routes/create-event.ts
@@ -0,0 +1,51 @@
+import { ZodTypeProvider } from "fastify-type-provider-zod"
+import z from "zod"
+import { generateSlug } from "../utils/generate-slug"
+import { FastifyInstance } from "fastify"
+import { } from "module";
+import { prisma } from "../server";
+
+export async function createEvent(app: FastifyInstance) {
+    app
+        .withTypeProvider<ZodTypeProvider>()
+        .post('/events', {
+            schema: {
+                body: z.object({
+                    title: z.string().min(4),
+                    details: z.string().nullable(),
+                    maximumAttndees: z.number().int().positive().nullable()
+                }),
+                response: {
+                    201: z.object({
+                        eventID: z.string().uuid()
+                    })
+                }
+            }
+        }, async (req, res) => {
+
+            const { title, details, maximumAttndees } = req.body
+
+            const slug = generateSlug(title)
+
+            const eventWithSemeSlug = await prisma.event.findUnique({
+                where: {
+                    slug
+                }
+            })
+
+            if (eventWithSemeSlug) {
+                throw new Error('Este evento ja esta registrado')
+            }
+
+            const event = await prisma.event.create({
+                data: {
+                    title,
+                    details,
+                    maximumAttndees,
+                    slug
+                }
+            })
+
+            return res.status(201).send({ eventID: event.id })
+        })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,57 +1,15 @@
-import { fastify } from "fastify";
-import { z } from "zod";
 import { PrismaClient } from "@prisma/client";
-import { generateSlug } from "./utils/generate-slug";
-import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
+import { fastify } from "fastify";
+import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
 
 const app = fastify()
-const prisma = new PrismaClient()
+export const prisma = new PrismaClient()
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app
-    .withTypeProvider<ZodTypeProvider>()
-    .post('/events', {
-        schema: {
-            body: z.object({
-                title: z.string().min(4),
-                details: z.string().nullable(),
-                maximumAttndees: z.number().int().positive().nullable()
-            }),
-            response: {
-                201: z.object({
-                    eventID: z.string().uuid()
-                })
-            }
-        }
-    }, async (req, res) => {
-
-        const { title, details, maximumAttndees } = req.body
-
-        const slug = generateSlug(title)
-
-        const eventWithSemeSlug = await prisma.event.findUnique({
-            where: {
-                slug
-            }
-        })
-
-        if (eventWithSemeSlug) {
-            throw new Error('Este evento ja esta registrado')
-        }
-
-        const event = await prisma.event.create({
-            data: {
-                title,
-                details,
-                maximumAttndees,
-                slug
-            }
-        })
-
-        return res.status(201).send({ eventID: event.id })
-    })
+app.register(createEvent)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('Servidor rodando')
